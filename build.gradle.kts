d06bc486f61ffd284516961ddcaa7e3bc63dddb5
kkyu8925 <kkyu8925@gmail.com>
2023-06-27 22:28:22 +0900

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,18 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // h2
     runtimeOnly("com.h2database:h2")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
