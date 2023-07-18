d07566b03cbc4137c6194af61043586f2a5cae20
kkyu8925 <kkyu8925@gmail.com>
2023-07-18 20:34:07 +0900

    섹션 7 - 더 나은 테스트를 작성하기 위한 구체적 조언, @ParameterizedTest

--- src/test/kotlin/sample/cafekiosk/spring/domain/product/ProductTypeTest.kt
+++ src/test/kotlin/sample/cafekiosk/spring/domain/product/ProductTypeTest.kt
@@ -3,6 +3,11 @@ package sample.cafekiosk.spring.domain.product
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class ProductTypeTest {
 
@@ -30,5 +35,39 @@ internal class ProductTypeTest {
 
         // then
         assertThat(result).isTrue()
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @CsvSource("HANDMADE,false", "BOTTLE,true", "BAKERY,true")
+    @ParameterizedTest
+    fun containsStockType4(productType: ProductType, expected: Boolean) {
+        // when
+        val result = ProductType.containsStockType(productType)
+
+        // then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @MethodSource("provideProductTypesForCheckingStockType")
+    @ParameterizedTest
+    fun containsStockType5(productType: ProductType, expected: Boolean) {
+        // when
+        val result = ProductType.containsStockType(productType)
+
+        // then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    companion object {
+
+        @JvmStatic
+        private fun provideProductTypesForCheckingStockType(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+            )
+        }
     }
 }
